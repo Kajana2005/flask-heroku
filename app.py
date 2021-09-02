2b255c8bccf1d718feaf2a1580fd58e0456b9cca
Kajana2005 <89174934+Kajana2005@users.noreply.github.com>
2021-09-02 12:27:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def get_api():
 
 @app.route(‘/hi’)
 @app.route(‘/hello’)
-def hi():
-    return "สวัสดี" 
+def  hi():
+    return  "สวัสดี" 
 
 if __name__ == "__main__":
     app.run(debug=False)
